dfcc0bc8c70d905e090fdd2d0960e8c7dbf1300e
James Post <james.richard.post@gmail.com>
2022-05-17 13:41:25 -0500

    ignore node modules

---
+++ src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { formatException } from "stylus/lib/utils";
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <form className='store-selector'>
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store</button>
+            </form>
+        )
+    }
+}
+
+export default StorePicker;
